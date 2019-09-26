fe19f0a4eed51fac11fc4bbc5cc8bb08dfb4ec81
abhishekvtangod <abhishekvtangod@gmail.com>
2019-09-27 01:30:03 +0530

    coderit workshop

---
+++ MLworkshop/ubuntu_setup.sh
@@ -0,0 +1,14 @@
+sudo -s
+apt-get install python3-dev
+apt install python3-pip
+pip3 install numpy
+pip3 install scipy
+pip3 install pandas
+pip3 install scikit-learn
+pip3 install nltk
+pip3 install gtts
+pip3 install gensim
+pip3 install jupyter
+pip3 install matplotlib
+pip3 install seaborn
+exit
